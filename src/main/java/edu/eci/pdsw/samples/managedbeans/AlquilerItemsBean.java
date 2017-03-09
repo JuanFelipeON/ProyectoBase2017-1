b5e20fa19ae87cfd1f512ddc8d73e4eed016d452
Juan Felipe Ortiz <jfelipeon@outlook.com>
2017-03-09 00:35:45 -0500

    Autor 1: Version uno vista de RegistrarClientes

--- src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/AlquilerItemsBean.java
@@ -5,8 +5,11 @@
  */
 package edu.eci.pdsw.samples.managedbeans;
 
+import edu.eci.pdsw.samples.entities.Cliente;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import java.io.Serializable;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -19,8 +22,78 @@ import javax.faces.bean.SessionScoped;
 public class AlquilerItemsBean implements Serializable {
 
     ServiciosAlquiler sp = ServiciosAlquiler.getInstance();
+    
+    public List<Cliente> listaClientes;
+    public Cliente cliente;
+    
+    public String nombre;
+    public long documento;
+    public String telefono;
+    public String direccion;
+    public String email;
 
     public AlquilerItemsBean() {
+        this.cliente = new Cliente();
+    }
+
+    public List<Cliente> getListaClientes() throws ExcepcionServiciosAlquiler {
+        return sp.consultarClientes();
+    }
+
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public long getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(long documento) {
+        this.documento = documento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public void clienteNuevo() throws ExcepcionServiciosAlquiler{
+        sp.registrarCliente(new Cliente(nombre,documento,telefono,direccion,email));
     }
 
 }
